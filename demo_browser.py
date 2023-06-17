c49d8177ce513680016ccd64b6f555d30a756c20
Adam Lapidoth <adamlapidoth@gmail.com>
2023-06-17 13:34:36 +0300

    demo_browser.py: more driver actions

--- demo_browser.py
+++ demo_browser.py
@@ -3,12 +3,18 @@ from selenium import webdriver
 # chrome_driver
 from selenium.webdriver.chrome.service import Service
 
-service_obj = (
-    Service(r"C:\Users\adaml\Documents\python\
-    selenium-python\chromedriver.exe")
+service_obj = Service(
+    r"C:\Users\adaml\Documents\python\
+    selenium-python\chromedriver.exe"
 )
 driver = webdriver.Chrome(service=service_obj)
+driver.maximize_window()
 driver.get("https://rahulshettyacademy.com")
 print(driver.title)
 print(driver.current_url)
+driver.get("https://rahulshettyacademy.com/selemiumPractice/#/")
+driver.minimize_window()
+driver.back()
+driver.refresh()
+driver.forward()
 driver.close()
