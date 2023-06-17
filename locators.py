56634d9a3a4bb112c3891033f85cd9a43637c4c1
Adam Lapidoth <adamlapidoth@gmail.com>
2023-06-17 15:34:55 +0300

    learnt more css selectors

--- locators.py
+++ locators.py
@@ -12,10 +12,16 @@ driver.find_element(By.NAME, "email").send_keys("hello@example.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID, "exampleCheck1").click()
 
-# CSS - tagname[attribute='value'] -? input[type='submit']
+# CSS - tagname[attribute='value'] -? input[type='submit'], #id, .classname
 driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Adam")
+driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
 # Xpath - //tagname[@attribute='value'] -? //input[@type='submit']
 driver.find_element(By.XPATH, "//input[@type='submit']").click()
 msg = driver.find_element(By.CLASS_NAME, "alert-success").text
 print(msg)
 assert "Success" in msg
+
+driver.find_element(By.XPATH, "(//input[@type='text'])[3]").send_keys(
+    "Hello Again!"
+)
+driver.find_element(By.XPATH, "(//input[@type='text'])[3]").clear()
