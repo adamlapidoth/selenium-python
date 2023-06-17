bb1e4ff3ef6f05136abda31cff6296471ebf5655
Adam Lapidoth <adamlapidoth@gmail.com>
2023-06-17 14:20:30 +0300

    demo_browser.py: added ability to run tests on Edge and Firefox

--- demo_browser.py
+++ demo_browser.py
@@ -2,12 +2,25 @@ from selenium import webdriver
 
 # chrome_driver
 from selenium.webdriver.chrome.service import Service
+# --Chrome
+# service_obj = Service(
+#     r"C:\Users\adaml\Documents\python\
+#     selenium-python\chromedriver.exe"
+# )
+# driver = webdriver.Chrome(service=service_obj)
 
+# --Firefox
 service_obj = Service(
-    r"C:\Users\adaml\Documents\python\
-    selenium-python\chromedriver.exe"
+    r"C:\Users\adaml\Documents\python\selenium-python\geckodriver.exe"
 )
-driver = webdriver.Chrome(service=service_obj)
+driver = webdriver.Firefox(service=service_obj)
+
+# --Edge
+# service_obj = Service(
+#     r"C:\Users\adaml\Documents\python\selenium-python\msedgedriver.exe"
+# )
+# driver = webdriver.Edge(service=service_obj)
+
 driver.maximize_window()
 driver.get("https://rahulshettyacademy.com")
 print(driver.title)
