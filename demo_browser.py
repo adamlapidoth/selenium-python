c1cc09ba7dd39b5699813bbe9acc1417989fc92d
Adam Lapidoth <adamlapidoth@gmail.com>
2023-06-17 14:20:48 +0300

    Revert "demo_browser.py: added ability to run tests on Edge and Firefox"
    This reverts commit bb1e4ff3ef6f05136abda31cff6296471ebf5655.

--- demo_browser.py
+++ demo_browser.py
@@ -2,25 +2,12 @@ from selenium import webdriver
 
 # chrome_driver
 from selenium.webdriver.chrome.service import Service
-# --Chrome
-# service_obj = Service(
-#     r"C:\Users\adaml\Documents\python\
-#     selenium-python\chromedriver.exe"
-# )
-# driver = webdriver.Chrome(service=service_obj)
 
-# --Firefox
 service_obj = Service(
-    r"C:\Users\adaml\Documents\python\selenium-python\geckodriver.exe"
+    r"C:\Users\adaml\Documents\python\
+    selenium-python\chromedriver.exe"
 )
-driver = webdriver.Firefox(service=service_obj)
-
-# --Edge
-# service_obj = Service(
-#     r"C:\Users\adaml\Documents\python\selenium-python\msedgedriver.exe"
-# )
-# driver = webdriver.Edge(service=service_obj)
-
+driver = webdriver.Chrome(service=service_obj)
 driver.maximize_window()
 driver.get("https://rahulshettyacademy.com")
 print(driver.title)
